a20353d33f65828d9b6b3c8c076b0f922952192c
tomkqwe <tomkqwe@gmail.com>
2022-04-01 19:19:04 +0300

    Add PropertiesUtil

--- src/main/java/ru/liga/prerevolutionarytindertgbotclient/App.java
+++ src/main/java/ru/liga/prerevolutionarytindertgbotclient/App.java
@@ -1,9 +1,14 @@
 package ru.liga.prerevolutionarytindertgbotclient;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import ru.liga.prerevolutionarytindertgbotclient.configuration.MyConfig;
+import ru.liga.prerevolutionarytindertgbotclient.entity.User;
 import ru.liga.prerevolutionarytindertgbotclient.telegrambot.RussianOldTinderBot;
+
+import java.util.List;
 
 /**
  * Start Bot!
@@ -13,11 +18,12 @@ public class App
 {
     public static void main( String[] args )
     {
+
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new RussianOldTinderBot());
         } catch (TelegramApiException e) {
-            throw new RuntimeException(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/ru/liga/prerevolutionarytindertgbotclient/telegrambot/RussianOldTinderBot.java
+++ src/main/java/ru/liga/prerevolutionarytindertgbotclient/telegrambot/RussianOldTinderBot.java
@@ -1,36 +1,68 @@
 package ru.liga.prerevolutionarytindertgbotclient.telegrambot;
 
+import lombok.SneakyThrows;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.liga.prerevolutionarytindertgbotclient.Communication;
+import ru.liga.prerevolutionarytindertgbotclient.configuration.MyConfig;
+import ru.liga.prerevolutionarytindertgbotclient.entity.User;
+import ru.liga.prerevolutionarytindertgbotclient.utils.PropertiesUtil;
 
 
 public class RussianOldTinderBot extends TelegramLongPollingBot {
+    private static final String BOT_USERNAME_KEY = "bot.name";
+    private static final String BOT_TOKEN_KEY = "bot.token";
     @Override
     public String getBotUsername() {
-        return "prerevolutionaryTinder_bot";
+        return PropertiesUtil.get(BOT_USERNAME_KEY);
     }
 
     @Override
     public String getBotToken() {
-        return "5192300654:AAF_oVYZy-qXsv5dFqzk2IyEt6SosBwpc8I";
+        return PropertiesUtil.get(BOT_TOKEN_KEY);
     }
 
 
+    @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
-            message.setChatId(update.getMessage().getChatId().toString());
-            message.setText(update.getMessage().getText());
+            Long chatId = update.getMessage().getChatId();
+
             try {
-                execute(message); // Call method to send the message
+
+                returnUserById(update, chatId);
+
             } catch (TelegramApiException e) {
-                e.printStackTrace();
+                System.out.println(e.getMessage());
+                throw new RuntimeException(e);
             }
+
         }
+    }
+
+    /**
+     * Берем месседж парсим айдишку
+     * @param update
+     * @param chatId
+     * @throws TelegramApiException
+     */
+    private void returnUserById(Update update, Long chatId) throws TelegramApiException {
+        String s = update.getMessage().getText();
+        String[] split = s.split(" ");
+        int i = Integer.parseInt(split[1]);
+        execute(new SendMessage(chatId.toString(), getUserById(i).toString()));
+    }
+
+    private User getUserById(int id) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        Communication communication = context.getBean("communication", Communication.class);
+        return communication.getUser(id);
     }
 
 }
